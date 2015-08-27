feff565fbddbeea922764bb8af2793020a0865b6
Fredrik Schultz <fschultz@users.noreply.github.com>
2015-08-27 08:03:22 +0200

    Adding default sort order to news lists

--- Models/Pages/NewsListPage.cs
+++ Models/Pages/NewsListPage.cs
@@ -1,6 +1,7 @@
 ﻿namespace DemoSite.Models.Pages {
     using KalikoCMS.Attributes;
     using KalikoCMS.Core;
+    using KalikoCMS.Core.Collections;
 
     /// <summary>
     /// This is a page type that will only act as a container and therefor has no additional properties.
@@ -9,6 +10,8 @@
     [PageType("NewsList", "News list", 
         PageTypeDescription = "Used for news archives", 
         AllowedTypes = new[] { typeof(NewsListPage), typeof(NewsPage) },
+        DefaultChildSortOrder = SortOrder.CreatedDate,
+        DefaultChildSortDirection = SortDirection.Descending,
         PreviewImage = "/Assets/Images/newslistpage.png")]
     public class NewsListPage : CmsPage {
     }
